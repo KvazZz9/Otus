90c14d77f6bbac8d137eba37306df5da3c178eb9
KvazZz9 <sperlinik666@gmail.com>
2025-08-20 09:33:31 +0300

    Add files via upload

---
+++ tofu/providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+
+      vsphere = {
+      source  = "registry.terraform.io/hashicorp/vsphere"
+      version = "2.11.1"
+                }
+       vault = {
+      source  = "registry.terraform.io/hashicorp/vault"
+      version = "3.24.0"
+               }
+                    }
+
+           }
+
+provider "vault" {
+  skip_child_token = true
+  address          = "адрес_вашей_валты:8200"
+  token            = var.VAULT_TOKEN
+}
+
+provider "vsphere" {
+  user              = data.vault_generic_secret.my_secret.data.vsphere_user
+  password          = data.vault_generic_secret.my_secret.data.vsphere_password
+  vsphere_server    = "vcenter.hayas.ru"
+  allow_unverified_ssl = true
+}
---
+++ tofu/vm_balancePG.tf
@@ -0,0 +1,89 @@
+resource "vsphere_virtual_machine" "balancepg01" {
+  name             = "balancepg01"
+  firmware         = "bios"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  folder           = "TEST/D.U."
+
+  num_cpus = 4
+  memory   = 8192
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label            = "disk0"
+    size             = 30
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+    unit_number      = 0	
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = "balance01"
+        domain    = "dc"
+      }
+
+      dns_server_list = ["192.168.3.4", "192.168.3.3"]
+
+      network_interface {
+        ipv4_address = "192.168.117.121"
+        ipv4_netmask = 24
+      }
+
+      ipv4_gateway = "192.168.117.1"
+    }
+  }
+}
+
+resource "vsphere_virtual_machine" "balancepg02" {
+  name             = "balancepg02"
+  firmware         = "bios"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  folder           = "TEST/D.U."
+
+  num_cpus = 4
+  memory   = 8192
+  guest_id = data.vsphere_virtual_machine.template.guest_id
+
+  network_interface {
+    network_id   = data.vsphere_network.network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+
+  disk {
+    label            = "disk0"
+    size             = 30
+    eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+    unit_number      = 0	
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+
+    customize {
+      linux_options {
+        host_name = "balancepg02"
+        domain    = "dc"
+      }
+
+      dns_server_list = ["192.168.3.4", "192.168.3.3"]
+
+      network_interface {
+        ipv4_address = "192.168.117.122"
+        ipv4_netmask = 24
+      }
+
+      ipv4_gateway = "192.168.117.1"
+    }
+  }
+}
